5c9a20ead48a87c2781a63b8d0d03fc2bdea82a5
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-21 10:57:09 +0400

    recursive solution

--- src/fop/w4binom/Bino.java
+++ src/fop/w4binom/Bino.java
@@ -1,26 +1,11 @@
 package fop.w4binom;
 
 public class Bino extends MiniJava {
-    public static int[][] pascal(int n) {
-
-        int [][] b = new int [n][n];
-        for (int i=0; i<n;i++)
-        {
-            b[i][0] =1;
-            b[i][i] = 1;
-            for(int j=1;j<i;j++)
-                b[i][j] = b[i-1][j-1] + b[i-1][j];
-        }
-        return b;
-    }
-
-
-
-
         public static int bino (int n, int k)
     {
-int [][] a= pascal(n+1);
-return a[n][k];
+if(n==k || k == 0) return 1;
+else return bino(n-1,k-1) + bino(n-1,k);
+
     }
 
 
